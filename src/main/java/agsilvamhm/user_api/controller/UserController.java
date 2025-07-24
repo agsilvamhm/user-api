19bf1de835ad549c84988e313418d4b6d03b9c52
Adalberto Gonçalves da Silva <agsilva.mhm@gmail.com>
2025-07-23 22:46:54 -0300

    Primeiro commit

---
+++ src/main/java/agsilvamhm/user_api/controller/UserController.java
@@ -0,0 +1,53 @@
+package agsilvamhm.user_api.controller;
+
+import agsilvamhm.user_api.dto.UserDto;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class UserController {
+    public static List<UserDto> usuarios = new ArrayList<>();
+
+    @GetMapping("/")
+    public String getMensagem(){
+        return "Spring boot is working!";
+    }
+
+    @PostConstruct
+    public void initiateList(){
+        UserDto userDTO = new UserDto();
+        userDTO.setNome("Eduardo");
+        userDTO.setCpf("123");
+        userDTO.setEndereco("Rua a");
+        userDTO.setEmail("eduardo@email.com");
+        userDTO.setTelefone("1234-3454");
+        userDTO.setDataCadastro(new Date());
+        UserDto userDTO2 = new UserDto();
+        userDTO2.setNome("Luiz");
+        userDTO2.setCpf("456");
+        userDTO2.setEndereco("Rua b");
+        userDTO2.setEmail("luiz@email.com");
+        userDTO2.setTelefone("1234-3454");
+        userDTO2.setDataCadastro(new Date());
+        UserDto userDTO3 = new UserDto();
+        userDTO3.setNome("Bruna");
+        userDTO3.setCpf("678");
+        userDTO3.setEndereco("Rua c");
+        userDTO3.setEmail("bruna@email.com");
+        userDTO3.setTelefone("1234-3454");
+        userDTO3.setDataCadastro(new Date());
+        usuarios.add(userDTO);
+        usuarios.add(userDTO2);
+        usuarios.add(userDTO3);
+    }
+
+    @GetMapping("/users")
+    public List<UserDto> getUsuarios(){
+        return usuarios;
+    }
+}
---
+++ src/test/java/agsilvamhm/user_api/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package agsilvamhm.user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
